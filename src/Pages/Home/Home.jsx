26825e470273d187873ea811eb0fe2262ef1b758
AhmedSaeed217 <ahmedsaeedfci@gmail.com>
2024-10-08 20:19:29 +0300

    Pages Folder and Home Page are created

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ImgSlider from "../../Components/ImgSlider/ImgSlider";
+import Features from "../../Components/Features/Features";
+import Products from "../../Components/Products/Products";
+function Home() {
+  return (
+    <div>
+      <ImgSlider />
+      <Features />
+      <Products />
+    </div>
+  );
+}
+
+export default Home;
