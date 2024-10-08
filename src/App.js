c7d381ad7091f3302ded5fa980b2359fdf5a24a6
AhmedSaeed217 <ahmedsaeedfci@gmail.com>
2024-10-08 20:20:41 +0300

    Routing is created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Features from "./Components/Features/Features";
 import Footer from "./Components/Footer/Footer";
-import ImgSlider from "./Components/ImgSlider/ImgSlider";
 import Navbar from "./Components/Navbar/Navbar";
-import Products from "./Components/Products/Products";
 import { CartProvider } from "./Context/CartContext";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <CartProvider>
-      <Navbar />
-      <ImgSlider />
-      <Features />
-      <Products />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </CartProvider>
   );
 }
