c523bb9093b3cdd6200ffbd34e401f18d44f373d
AhmedSaeed217 <ahmedsaeedfci@gmail.com>
2024-10-14 20:01:37 +0300

    React Hot Toast is Installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import { CartProvider } from "./Context/CartContext";
 import Home from "./Pages/Home/Home";
+import ProductDetails from "./Components/ProductDetails/ProductDetails";
+import Cart from "./Pages/Cart/Cart";
 function App() {
   return (
     <CartProvider>
@@ -11,8 +15,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products/:productId" element={<ProductDetails />} />
         </Routes>
         <Footer />
+        <Toaster />
       </BrowserRouter>
     </CartProvider>
   );
