936f0385c359fe289fc236b3b7ba43e0cfd33be4
AhmedSaeed217 <ahmedsaeedfci@gmail.com>
2024-10-30 21:24:17 +0300

    Slider Component is Changed

--- src/Components/ImgSlider/ImgSlider.jsx
+++ src/Components/ImgSlider/ImgSlider.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 import "./ImgSlider.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import slider1 from "../../assets/images/slider1.jpg";
-import slider2 from "../../assets/images/slider2.jpg";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import slider1 from "../../assets/images/slider1.jpg";
+// import slider2 from "../../assets/images/slider2.jpg";
 function ImgSlider() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    accessibility: true,
-    autoplay: true,
-  };
+  // var settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   accessibility: true,
+  //   autoplay: true,
+  // };
 
-  return (
-    <div className="slider">
-      {/* <div className="container"> */}
-      <Slider {...settings}>
-        <div>
-          <img className="slider-img" src={slider1} alt="" />
-        </div>
-        <div>
-          <img className="slider-img" src={slider2} alt="" />
-        </div>
-      </Slider>
-      {/* </div> */}
+  return <>
+  <div className="hero">
+    <div className="container">
+    {/* <h2>Explore Our Products</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident autem non consectetur praesentium impedit ut unde incidunt beatae ex velit dolorum quibusdam cupiditate quasi officia, aliquam accusamus alias nam!</p>
+     */}
     </div>
-  );
+  </div>
+  
+  </>
+    // <div className="slider">
+    //   {/* <div className="container"> */}
+    //   <Slider {...settings}>
+    //     <div>
+    //       <img className="slider-img" src={slider1} alt="" />
+    //     </div>
+    //     <div>
+    //       <img className="slider-img" src={slider2} alt="" />
+    //     </div>
+    //   </Slider>
+    //   {/* </div> */}
+    // </div>
+  
 }
 
 export default ImgSlider;
